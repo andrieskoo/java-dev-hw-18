822ca5c55eab900bd767f13c0815963ee77a13fc
andrieskoo <andrieskox@gmail.com>
2024-05-26 11:48:00 +0200

    commiting for doing pull

--- src/main/java/com/homework18/exception/IllegalArgumentException.java
+++ src/main/java/com/homework18/exception/IllegalArgumentException.java
@@ -2,7 +2,7 @@ package com.homework18.exception;
 
 
 public class IllegalArgumentException extends Exception{
-    private static final String ILLEGAL_ARGUMENT_PASSED_TO_METHOD_TEXT = "Illegal or unsuitable argument passed to a method, argument is %s";
+    private static final String ILLEGAL_ARGUMENT_PASSED_TO_METHOD_TEXT = "Illegal or unsuitable argument passed to a method, argument is %s ";
     private static final String ILLEGAL_ARGUMENT = "Illegal argument exception. %s";
 
 
